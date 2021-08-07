923df195fcdc0e4f84094c68ba598af30644fa6d
Julio Cesar Aguilar Cabrera <julio.aguilar@lania.edu.mx>
2021-08-07 13:31:13 -0500

    Dos nuevas pruebas de ciudades

--- src/test/java/mx/lania/ejemplo/ciudades/control/ControladorCatalogosTest.java
+++ src/test/java/mx/lania/ejemplo/ciudades/control/ControladorCatalogosTest.java
@@ -1,14 +1,20 @@
 package mx.lania.ejemplo.ciudades.control;
 
+import java.net.URISyntaxException;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -19,6 +25,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ControladorCatalogosTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ControladorCatalogosTest.class);
+
     @Autowired
     TestRestTemplate restTemplate;
 
@@ -26,16 +34,15 @@ public class ControladorCatalogosTest {
     private String puerto;
 
     private static String urlBase;
-    
+
     @Before
     public void prepararUrl() {
         this.urlBase = "http://localhost:" + puerto;
     }
 
-    
     @Test
     public void testGetPaises() {
-        //LOGGER.debug("testGetPaises");
+        LOGGER.debug("testGetPaises");
         String resultado = restTemplate.getForObject(this.urlBase + "/countries", String.class);
         Assertions.assertThat(resultado)
                 .startsWith("[{")
@@ -43,5 +50,23 @@ public class ControladorCatalogosTest {
                 .contains("Antigua");
     }
 
-    
+    @Test
+    public void testGetCiudadExistente() {
+        LOGGER.debug("testGetCiudadExistente");
+        String resultado = restTemplate.getForObject(this.urlBase + "/cities/1", String.class);
+        Assertions.assertThat(resultado)
+                .startsWith("{")
+                .endsWith("}")
+                .contains("Kabul");
+    }
+
+    @Test
+    public void testGetCiudadNoExistente() throws URISyntaxException {
+        LOGGER.debug("testGetCiudadNoExistente");
+        HttpEntity<String> peticion = new HttpEntity<>("");
+        ResponseEntity<String> resultado = restTemplate.exchange(this.urlBase + "/cities/111", HttpMethod.GET, peticion, String.class);
+        Assertions.assertThat(resultado.getStatusCodeValue())
+                .isEqualTo(404);
+    }
+
 }
